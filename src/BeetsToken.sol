d9954ca6adff1410670e7bba31f79d2bbfa5d7a8
franz <franzns11@gmail.com>
2024-12-09 12:09:35 +0100

    init

--- src/BeetsToken.sol
+++ src/BeetsToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
 contract Beets is ERC20, Ownable {
     uint256 public constant YEAR_IN_SECONDS = 365 days;
@@ -26,7 +26,7 @@ contract Beets is ERC20, Ownable {
 
     constructor(uint256 _initialSupply)
         ERC20("Beets", "BEETS")
-        Ownable()
+        Ownable(msg.sender)
     {
         _mint(msg.sender, _initialSupply);
 
