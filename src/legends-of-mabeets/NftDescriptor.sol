d0bf17fe67b5989b04f799a39a3c7b40324e2882
franz <franzns11@gmail.com>
2025-03-03 23:17:20 +0100

    rename and make uri adaptable

---
+++ src/legends-of-mabeets/NftDescriptor.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import "./INFTDescriptor.sol";
+import "./LegendsOfMaBeets.sol";
+
+contract NftDescriptor is INFTDescriptor {
+    using Strings for uint256;
+
+    string private constant S3 = "https://beethoven-assets.s3.eu-central-1.amazonaws.com/";
+
+    LegendsOfMaBeets public immutable legendsContract;
+
+    constructor(LegendsOfMaBeets lom) {
+        legendsContract = lom;
+    }
+
+    /// @notice Returns a link to the stored image
+    function constructTokenURI(uint256 tokenId) external view override returns (string memory uri) {
+        uri = string.concat(S3, "mabeets-legends.png");
+    }
+}
---
+++ test/LegendsOfMaBeets.t.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import {LegendsOfMaBeets} from "src/legends-of-mabeets/LegendsOfMaBeets.sol";
+import {NftDescriptor} from "src/legends-of-mabeets/NftDescriptor.sol";
+
+contract LegendsOfMaBeetsTest is Test {
+    string SONIC_FORK_URL = "https://rpc.soniclabs.com";
+    uint256 INITIAL_FORK_BLOCK_NUMBER = 10505083;
+    uint256 sonicFork;
+
+    LegendsOfMaBeets lomNftContract;
+
+    error OwnableUnauthorizedAccount(address account);
+
+    function setUp() public {
+        sonicFork = vm.createSelectFork(SONIC_FORK_URL, INITIAL_FORK_BLOCK_NUMBER);
+
+        lomNftContract = new LegendsOfMaBeets();
+        NftDescriptor lomNftDescriptor = new NftDescriptor(lomNftContract);
+        lomNftContract.setNFTDescriptor(lomNftDescriptor);
+    }
+
+    function testConstructor() public view {
+        assertEq(lomNftContract.name(), "Legends of maBeets");
+        assertEq(lomNftContract.symbol(), "LOM");
+    }
+
+    function testMint() public {
+        uint256 amount = 100 ether;
+        uint256 level = 10;
+
+        address owner = vm.addr(1);
+
+        uint256 id = lomNftContract.mint(owner, level, amount);
+        LegendsOfMaBeets.PositionInfo memory position = lomNftContract.getPositionForId(id);
+
+        assertEq(position.amount, amount);
+        assertEq(position.level, level);
+        assertEq(lomNftContract.ownerOf(id), owner);
+        assertEq(lomNftContract.totalSupply(), 1);
+    }
+
+    function testMultiMint() public {
+        uint256 amount1 = 100 ether;
+        uint256 level1 = 10;
+        uint256 amount2 = 200 ether;
+        uint256 level2 = 5;
+        uint256 amount3 = 120 ether;
+        uint256 level3 = 3;
+
+        address owner1 = vm.addr(1);
+        address owner2 = vm.addr(2);
+
+        uint256 id1 = lomNftContract.mint(owner1, level1, amount1);
+        LegendsOfMaBeets.PositionInfo memory position1 = lomNftContract.getPositionForId(id1);
+
+        assertEq(position1.amount, amount1);
+        assertEq(position1.level, level1);
+        assertEq(lomNftContract.ownerOf(id1), owner1);
+        assertEq(lomNftContract.totalSupply(), 1);
+
+        uint256 id2 = lomNftContract.mint(owner1, level2, amount2);
+        LegendsOfMaBeets.PositionInfo memory position2 = lomNftContract.getPositionForId(id2);
+
+        assertEq(position2.amount, amount2);
+        assertEq(position2.level, level2);
+        assertEq(lomNftContract.ownerOf(id2), owner1);
+        assertEq(lomNftContract.totalSupply(), 2);
+
+        uint256 id3 = lomNftContract.mint(owner2, level3, amount3);
+        LegendsOfMaBeets.PositionInfo memory position3 = lomNftContract.getPositionForId(id3);
+
+        assertEq(position3.amount, amount3);
+        assertEq(position3.level, level3);
+        assertEq(lomNftContract.ownerOf(id3), owner2);
+        assertEq(lomNftContract.totalSupply(), 3);
+    }
+
+    function testTransfer() public {
+        uint256 amount1 = 100 ether;
+        uint256 level1 = 10;
+
+        address owner1 = vm.addr(1);
+        address owner2 = vm.addr(2);
+
+        uint256 id1 = lomNftContract.mint(owner1, level1, amount1);
+        LegendsOfMaBeets.PositionInfo memory position1 = lomNftContract.getPositionForId(id1);
+
+        assertEq(position1.amount, amount1);
+        assertEq(position1.level, level1);
+        assertEq(lomNftContract.ownerOf(id1), owner1);
+        assertEq(lomNftContract.totalSupply(), 1);
+
+        vm.prank(owner1);
+        lomNftContract.safeTransferFrom(owner1, owner2, id1);
+        assertEq(lomNftContract.ownerOf(id1), owner2);
+    }
+
+    function testOnlyOwnerCanMint() public {
+        address user = vm.addr(1);
+
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(user)));
+        lomNftContract.mint(user, 10, 1000 ether);
+    }
+
+    function testNftTokenURI() public {
+        uint256 amount = 100 ether;
+        uint256 level = 10;
+
+        address owner = vm.addr(1);
+
+        uint256 id = lomNftContract.mint(owner, level, amount);
+        string memory tokenURI = lomNftContract.tokenURI(id);
+
+        assertEq(tokenURI, "https://beethoven-assets.s3.eu-central-1.amazonaws.com/mabeets-legends.png");
+    }
+
+    function testNonExistentTokenUri() public {
+        uint256 id = 1;
+        vm.expectRevert(abi.encodeWithSelector(LegendsOfMaBeets.NonExistentToken.selector));
+        lomNftContract.tokenURI(id);
+    }
+
+    function testSetNFTDescriptor() public {
+        NftDescriptor newDescriptor = new NftDescriptor(lomNftContract);
+        lomNftContract.setNFTDescriptor(newDescriptor);
+
+        assertEq(address(lomNftContract.nftDescriptor()), address(newDescriptor));
+    }
+
+    function testSetNFTDescriptorOnlyOwner() public {
+        NftDescriptor newDescriptor = new NftDescriptor(lomNftContract);
+        address user = vm.addr(1);
+
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(user)));
+        lomNftContract.setNFTDescriptor(newDescriptor);
+    }
+
+    function testGetPositionForId() public {
+        uint256 amount = 100 ether;
+        uint256 level = 10;
+
+        address owner = vm.addr(1);
+
+        uint256 id = lomNftContract.mint(owner, level, amount);
+        LegendsOfMaBeets.PositionInfo memory position = lomNftContract.getPositionForId(id);
+
+        assertEq(position.amount, amount);
+        assertEq(position.level, level);
+    }
+
+    function testGetPositionForIdNonExistent() public {
+        uint256 id = 1;
+        LegendsOfMaBeets.PositionInfo memory position = lomNftContract.getPositionForId(id);
+
+        assertEq(position.amount, 0);
+        assertEq(position.level, 0);
+    }
+}
